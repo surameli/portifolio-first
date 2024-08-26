9ea79c6a59c4d363052a52d8c571089f1380900f
surameli <167941245+surameli@users.noreply.github.com>
2024-08-27 01:35:01 +0300

    header and banner

---
+++ sura-portifolio-second/src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+
+import Profile from '../../components/Banner/Profile/Profile'
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    {/* <Banner/>
+    <Footer/> */}
+    <Profile/>
+    </>
+  )
+}
+
+export default Home
