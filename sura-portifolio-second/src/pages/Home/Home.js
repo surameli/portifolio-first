9af3376490eed496d296400ee1af650a48f6accd
surameli <167941245+surameli@users.noreply.github.com>
2024-08-28 22:40:05 +0300

    about me

--- sura-portifolio-second/src/pages/Home/Home.js
+++ sura-portifolio-second/src/pages/Home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
-
 import Profile from '../../components/Banner/Profile/Profile'
+import AboutMe from '../../components/AboutMe/AboutMe'
 const Home = () => {
   return (
     <>
     <Header/>
-    {/* <Banner/>
-    <Footer/> */}
     <Profile/>
+    <AboutMe/>
     </>
   )
 }
